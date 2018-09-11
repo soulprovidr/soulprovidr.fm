d0b80bbdf0ad3be116b6ebdc5fcf9c7275efe831
Shola Anozie <shola@soulprovidr.co>
2018-09-11 02:09:19 -0500

    Fix for fetch import.

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import 'whatwg-fetch'
+
+exports.onClientEntry = () => true;
--- src/components/player.js
+++ src/components/player.js
@@ -1,4 +1,3 @@
-import 'whatwg-fetch';
 import React, { Component } from 'react';
 import { Howl, Howler } from 'howler';
 
