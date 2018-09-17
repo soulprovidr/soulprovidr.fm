17accf769f2f1a09c7271e725a8045ef555d65b4
Shola Anozie <shola@soulprovidr.co>
2018-09-17 02:37:13 -0500

    Testing node process.

--- src/api/main.js
+++ src/api/main.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const Queue = require('./queue');
 
-Queue.process((job, done) => {
-  fs.writeFileSync('./metadata.txt', job.data, 'utf8');
-  return done();
-});
+while (true) {
+  Queue.process((job, done) => {
+    fs.writeFileSync('./metadata.txt', job.data, 'utf8');
+    return done();
+  });
+}
