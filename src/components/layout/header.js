bc44b0d4d934f42b0adeb64aa0872ae47a569152
Shola Anozie <shola@soulprovidr.io>
2019-10-18 01:16:50 -0500

    Added social media icons.

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import GithubIcon from '../../static/images/github.svg';
+import InstagramIcon from '../../static/images/instagram.svg';
+import SpotifyIcon from '../../static/images/spotify.svg';
 import LogoImage from '../../static/images/logo.svg';
 
 const StyledHeader = styled.header`
@@ -13,37 +16,76 @@ const Logo = styled.img`
   height: 35px;
 `;
 
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const NavItem = styled.li`
+  font-weight: 600;
+`;
+
 export default function Header() {
   return (
-    <>
-      <StyledHeader className="bg-white py-4">
-        <div className="d-flex align-items-center py-2 px-4 container">
+    <StyledHeader className="bg-white py-4">
+      <div className="d-flex align-items-center justify-content-between py-2 px-4 container">
+        <div className="d-flex align-items-center">
           <Logo
             className="d-inline-block align-middle rounded-circle mr-3"
             src={LogoImage}
           />
-          <div>
-            <p className="h5 font-weight-bold m-0">
-              SOUL PROVIDER
-            </p>
+          <p className="h5 font-weight-bold m-0">
+            SOUL PROVIDER
+          </p>
+        </div>
+        <nav>
+          <div className="d-flex align-items-center">
+            <a
+              href="https://instagram.com/soulprovidr"
+              target="_blank"
+            >
+              <Icon
+                className="mr-3"
+                src={InstagramIcon}
+              />
+            </a>
+            <a
+              href="https://open.spotify.com/user/soulprovidr"
+              target="_blank"
+            >
+              <Icon
+                className="mr-3"
+                src={SpotifyIcon}
+              />
+            </a>
+            <a
+              href="https://github.com/soulprovidr/soulprovidr.fm"
+              target="_blank"
+            >
+              <Icon
+                className="mr-3"
+                src={GithubIcon}
+              />
+            </a>
+            <ul className="m-0">
+              <NavItem className="h7 d-inline-block text-uppercase pr-4">
+                Newsletter
+              </NavItem>
+              <NavItem className="h7 d-inline-block text-uppercase pr-4">
+                <a 
+                  href="https://reddit.com/r/rnb"
+                  target="_blank"
+                >
+                  Community
+                </a>
+              </NavItem>
+              <NavItem className="h7 d-inline-block text-uppercase">
+                Contact
+              </NavItem>
+            </ul>
           </div>
-        </div>
-      </StyledHeader>
-      {/* <StyledNav className="bg-white position-fixed">
-        <div className="d-flex align-items-center py-2 px-4 container">
-          <ul>
-            <li className="d-inline-block">
-              Radio
-            </li>
-            <li className="d-inline-block">
-              Mixtapes
-            </li>
-            <li className="d-inline-block">
-              Articles
-            </li>
-          </ul>
-        </div>
-      </StyledNav> */}
-    </>
+        </nav>
+      </div>
+    </StyledHeader>
   );
 }
