3ea605c4ecb384248adb5e502ab4c87b991ebebb
Shola Anozie <shola@soulprovidr.dev>
2020-01-12 17:48:34 -0600

    Updated About page template.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import get from 'lodash/get'
-import Img from 'gatsby-image'
+import React from 'react';
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
+import get from 'lodash/get';
+import Img from 'gatsby-image';
 
 function About(props) {
-    const post = get(props, 'data.contentfulPage')
-
+  const post = get(props, 'data.contentfulPage')
   return (
     <div className="container">
       <Helmet title={post.title} />
-      <div className="">
-        <Img className="card-img-top" alt={post.title} sizes={post.heroImage.sizes} />
-        <div className="card-body">
-          <div className="row pt-3">
-            <div className="col-md-8">
-              <p className="h2 pb-3">{post.title}</p>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: post.body.childMarkdownRemark.html,
-                }}
-              />
-            </div>
-          </div>
+      <div className="row">
+        <div className="col-md-4">
+          <Img className="card-img-top" alt={post.title} sizes={post.heroImage.sizes} />
+        </div>
+        <div className="col-md-8">
+          <p className="h2 font-weight-bold pb-3">{post.title}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.body.childMarkdownRemark.html,
+            }}
+          />
         </div>
       </div>
     </div>
