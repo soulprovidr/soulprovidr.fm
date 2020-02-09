beaede8196a3cfaefabb8f0ed3f5dbb8760616b8
Shola Anozie <shola@soulprovidr.dev>
2020-02-09 12:13:42 -0600

    SW fixes.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,21 +1,15 @@
-function cacheForDomain(domain) {
-  return function (e) {
-    if (!e.request.url.match(`/^.*(${domain}).*$/`)) {
-      return false;
-    }
-    e.respondWith(
-      caches.open(domain).then(function (cache) {
-        return cache.match(e.request).then(function (response) {
-          return response || fetch(e.request).then(function (response) {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      })
-    )
-  }
-}
-
 self.addEventListener('fetch', function (e) {
-  cacheForDomain('soundcloud.com')(e);
+  if (!e.request.url.match(/^.*(soundcloud.com).*$/)) {
+    return false;
+  }
+  e.respondWith(
+    caches.open('soundcloud').then(function (cache) {
+      return cache.match(e.request).then(function (response) {
+        return response || fetch(e.request).then(function (response) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      })
+    })
+  )
 });
