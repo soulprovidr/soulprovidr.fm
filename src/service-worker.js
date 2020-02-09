682d63179cea2b4bc675e85856307e9d141f17d3
Shola Anozie <shola@soulprovidr.dev>
2020-02-09 14:50:13 -0600

    No longer caching audio streams (for now).

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,18 @@
+const fetchAndCache = (e, cache) => {
+  fetch(e.request).then(function (response) {
+    cache.put(e.request, response.clone());
+    return response;
+  });
+}
+
 self.addEventListener('fetch', function (e) {
-  if (!e.request.url.match(/^.*(soundcloud.com).*$/)) {
+  if (!e.request.url.match(/^.*(soundcloud.com\/(resolve|tracks)).*$/)) {
     return false;
   }
   e.respondWith(
     caches.open('soundcloud').then(function (cache) {
       return cache.match(e.request).then(function (response) {
-        return response || fetch(e.request).then(function (response) {
-          cache.put(e.request, response.clone());
-          return response;
-        });
+        return response || fetchAndCache(e, cache);
       })
     })
   )
