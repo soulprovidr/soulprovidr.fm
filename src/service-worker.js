6ece9d05def711dcfa0bca4f9d8e53666e6f362d
Shola Anozie <shola@soulprovidr.dev>
2020-02-09 15:20:54 -0600

    Fix for previous commit.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,16 @@
 const fetchAndCache = (e, cache) => {
-  fetch(e.request).then(function (response) {
+  return fetch(e.request).then(function (response) {
     cache.put(e.request, response.clone());
     return response;
   });
 }
 
 self.addEventListener('fetch', function (e) {
-  if (!e.request.url.match(/^.*(soundcloud.com\/(resolve|tracks)).*$/)) {
+  const streamRegex = /^.*soundcloud.com\/tracks\/\d*\/stream.*$/;
+  if (!e.request.url.includes('soundcloud.com')) {
+    return false;
+  }
+  if (e.request.url.match(streamRegex)) {
     return false;
   }
   e.respondWith(
