ef56dba35495368aafff41b9724393d957c7bc44
Shola Anozie <shola@soulprovidr.io>
2020-03-15 17:38:27 -0500

    Created playerSlice.

--- src/player/index.js
+++ src/player/index.js
@@ -1,5 +1,50 @@
 import React, { useContext } from 'react';
+import { createSlice } from '@reduxjs/toolkit';
 import PlayerStore from './store';
+
+export const PlayerStatus = {
+  UNSTARTED: -1,
+  BUFFERING: 0,
+  PLAYING: 1,
+  PAUSED: 2,
+  ENDED: 3,
+  STOPPED: 4
+};
+
+const {
+  UNSTARTED,
+  BUFFERING,
+  PLAYING,
+  PAUSED,
+  ENDED,
+  STOPPED
+} = PlayerStatus;
+
+const initialState = {
+  duration: null,
+  meta: {},
+  progress: null,
+  status: UNSTARTED,
+  src: null
+};
+
+export const playerSlice = createSlice({
+  name: 'player',
+  initialState,
+  reducers: {
+    play: {
+      reducer(state, action) {
+        
+      },
+      prepare(src = null, meta = null) {
+        if (!src && !meta) {
+          return 
+        }
+        return { payload: { src } }
+      }
+    }
+  }
+})
 
 const PlayerContext = React.createContext(null);
 
