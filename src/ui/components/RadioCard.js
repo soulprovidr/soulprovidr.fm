9ee285ed0974afaa24a0d4a58fc06b3b2dfe4d51
Shola Anozie <shola@soulprovidr.dev>
2020-09-19 19:20:58 -0500

    Added hover styles to RadioCard.

--- src/ui/components/RadioCard.js
+++ src/ui/components/RadioCard.js
@@ -1,36 +1,65 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import get from 'lodash.get';
-
-import DefaultCover from 'ui/static/images/default.png';
-import { RadioUrl, getMeta as getRadioMeta } from 'modules/radio';
-import { Box, Flex, Heading, Text } from 'theme';
-
 import { PlayerStatus } from 'modules/player/constants';
 import { useClickAction } from 'modules/player/hooks';
 import StatusIndicator from 'modules/player/components/StatusIndicator';
+import { RadioUrl, getMeta as getRadioMeta } from 'modules/radio';
+import { Box, Flex, Heading, Text } from 'theme';
+import DefaultCover from 'ui/static/images/default.png';
 
 const { BUFFERING, PLAYING } = PlayerStatus;
 
+const transitionStyles = {
+  transition: 'transform 200ms cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+  '&:hover': {
+    transform: 'translate3d(0, -2px, 0)'
+  }
+};
+
 const StyledRadioCard = (props) => (
   <Flex
+    flexDirection={['column', 'row']}
     sx={{
       cursor: 'pointer',
-      flexDirection: ['column', 'row']
+      ...transitionStyles
     }}
     {...props}
   />
 );
 
-const RadioCard = (props) => {
+const StyledRadioCardImage = (props) => (
+  <Box mr={[0, 5]} width={[1, 1 / 3]}>
+    <Box as="img" borderRadius={0} width={1} {...props} />
+  </Box>
+);
+
+const StyledRadioCardContent = (props) => (
+  <Flex
+    flexDirection="column"
+    justifyContent="center"
+    overflow="hidden"
+    mt={3}
+    width={[1, 2 / 3]}
+    {...props}
+  />
+);
+
+const StyledRadioCardTitle = (props) => (
+  <Heading
+    as="h1"
+    pb={1}
+    sx={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+    {...props}
+  />
+);
+
+const RadioCard = () => {
   const meta = useSelector(getRadioMeta);
-  // const progress = useSelector(getProgress);
-  // const status = useSelector(getStatus);
 
   const getMetaProperty = (property) => get(meta, property, null);
   const artist = getMetaProperty('artist');
   const cover = getMetaProperty('cover');
-  // const duration = getMetaProperty('duration');
   const title = getMetaProperty('title');
   const imageAlt = meta ? `${artist} - ${title}` : 'Loading...';
 
@@ -45,41 +74,13 @@ const RadioCard = (props) => {
     false
   );
 
-  const titleStyles = {
-    // mt: 3,
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis'
-  };
-
-  const imageStyles = {
-    borderRadius: 0,
-    width: '100%',
-    mr: [0, 4]
-  };
-  const image = (
-    <Box as="img" src={cover ?? DefaultCover} alt={imageAlt} sx={imageStyles} />
-  );
-
   return (
     <StyledRadioCard onClick={onClick}>
-      <Box width={[1, 1 / 3]}>{image}</Box>
-      <Flex
-        flexDirection="column"
-        justifyContent="center"
-        overflow="hidden"
-        ml={[0, 5]}
-        mt={3}
-        width={[1, 2 / 3]}
-      >
-        <Box>
-          <Heading as="h1" pb={1} sx={titleStyles}>
-            {title ?? 'Loading...'}
-          </Heading>
-          <Text fontSize={5} m={0}>
-            {artist}
-          </Text>
-        </Box>
-      </Flex>
+      <StyledRadioCardImage src={cover ?? DefaultCover} alt={imageAlt} />
+      <StyledRadioCardContent>
+        <StyledRadioCardTitle>{title ?? 'Loading...'}</StyledRadioCardTitle>
+        <Text fontSize={5}>{artist}</Text>
+      </StyledRadioCardContent>
     </StyledRadioCard>
   );
 };
