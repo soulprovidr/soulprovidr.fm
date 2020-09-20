5dd9c4eee85ef5f50491f6dfe99547ae2c4c289a
Shola Anozie <shola@soulprovidr.dev>
2020-09-19 20:42:10 -0500

    Removed reference to Bootstrap.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import store from '@/store';
 import { Layout } from 'ui/layout';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
