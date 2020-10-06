55bb8dcc4b3737e2f5cf27e4e2e9d893cc7b2ff7
Shola Anozie <shola@soulprovidr.fm>
2020-10-05 23:16:47 -0500

    Fix for active link classname on first load. (#24)
    * Active navigation link is now bold.
    
    * Removed underline from navigation links on hover.
    
    * Added partiallyActive prop to navigation Link components.
    
    * Fix for active link classname on first load.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '@/store';
-import { Layout } from 'ui/layout';
 
 export const registerServiceWorker = () => true;
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
 
 export const wrapRootElement = ({ element }) => (
   <Provider store={store}>{element}</Provider>
