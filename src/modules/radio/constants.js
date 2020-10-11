2282c37765f6bf7e3b8770cab2003eb19f5531a1
Shola Anozie <shola@soulprovidr.dev>
2020-10-11 14:11:59 -0500

    Updated stream URL.

--- src/modules/radio/constants.js
+++ src/modules/radio/constants.js
@@ -1,3 +1,3 @@
-export const RadioUrl = 'https://www.radioking.com/play/soul-provider-fm';
+export const RadioUrl = 'https://www.radioking.com/play/soulprovidr';
 export const RadioMetaUrl =
   'https://www.radioking.com/widgets/api/v1/radio/210013/track/current';
