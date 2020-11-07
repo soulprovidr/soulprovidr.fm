e4a42f96f4ea02f2bd2b8918adcf2487a24e4b70
Shola Anozie <shola@soulprovidr.io>
2020-11-07 12:52:59 -0600

    Renamed PANELBEAR_SITE_ID -> GATSBY_PANELBEAR_SITE_ID.

--- src/modules/analytics/middleware/panelBear.js
+++ src/modules/analytics/middleware/panelBear.js
@@ -4,12 +4,12 @@
 export const panelBearMiddleware = () => {
   // Only run in the browser.
   if (typeof window !== 'undefined') {
-    const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
+    const GATSBY_PANELBEAR_SITE_ID = process.env.GATSBY_PANELBEAR_SITE_ID;
 
     const createScriptTag = () => {
       const script = document.createElement('script');
       script.async = true;
-      script.src = `https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`;
+      script.src = `https://cdn.panelbear.com/analytics.js?site=${GATSBY_PANELBEAR_SITE_ID}`;
       return script;
     };
 
@@ -21,7 +21,7 @@ export const panelBearMiddleware = () => {
         window.panelbearQ = window.panelbearQ || [];
         window.panelbearQ.push(arguments);
       };
-    window.panelbear('config', { site: PANELBEAR_SITE_ID });
+    window.panelbear('config', { site: GATSBY_PANELBEAR_SITE_ID });
   }
   return (next) => (action) => next(action);
 };
