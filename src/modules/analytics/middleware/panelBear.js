c302859e60067e8a9a0e2b637b584faa58d4fef1
Shola Anozie <shola@soulprovidr.fm>
2020-11-07 12:56:41 -0600

    Fixed issue where PANELBEAR_SITE_ID was undefined. (#39)
    * Added PanelBear analytics.
    
    * Attempt to fix environment variable issue.
    
    * Renamed PANELBEAR_SITE_ID -> GATSBY_PANELBEAR_SITE_ID.

--- src/modules/analytics/middleware/panelBear.js
+++ src/modules/analytics/middleware/panelBear.js
@@ -1,18 +1,18 @@
-const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
-
-const createScriptTag = () => {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`;
-  return script;
-};
-
 /**
  * Inject PanelBear script into page.
  */
 export const panelBearMiddleware = () => {
   // Only run in the browser.
   if (typeof window !== 'undefined') {
+    const GATSBY_PANELBEAR_SITE_ID = process.env.GATSBY_PANELBEAR_SITE_ID;
+
+    const createScriptTag = () => {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = `https://cdn.panelbear.com/analytics.js?site=${GATSBY_PANELBEAR_SITE_ID}`;
+      return script;
+    };
+
     const script = createScriptTag();
     document.body.appendChild(script);
     window.panelbear =
@@ -21,7 +21,7 @@ export const panelBearMiddleware = () => {
         window.panelbearQ = window.panelbearQ || [];
         window.panelbearQ.push(arguments);
       };
-    window.panelbear('config', { site: PANELBEAR_SITE_ID });
+    window.panelbear('config', { site: GATSBY_PANELBEAR_SITE_ID });
   }
   return (next) => (action) => next(action);
 };
