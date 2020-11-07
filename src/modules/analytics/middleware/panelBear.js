b4d2d6168fa1ea5dc9a3ec564452222dcf14cf36
Shola Anozie <shola@soulprovidr.io>
2020-11-07 12:35:04 -0600

    Attempt to fix environment variable issue.

--- src/modules/analytics/middleware/panelBear.js
+++ src/modules/analytics/middleware/panelBear.js
@@ -1,18 +1,18 @@
-const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
-
-const createScriptTag = () => {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`;
-  return script;
-};
-
 /**
  * Inject PanelBear script into page.
  */
 export const panelBearMiddleware = () => {
   // Only run in the browser.
   if (typeof window !== 'undefined') {
+    const PANELBEAR_SITE_ID = process.env.PANELBEAR_SITE_ID;
+
+    const createScriptTag = () => {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = `https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`;
+      return script;
+    };
+
     const script = createScriptTag();
     document.body.appendChild(script);
     window.panelbear =
