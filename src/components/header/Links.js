12f47449778db30e06f87ab40040cb3cd8f37145
Shola Anozie <shola@soulprovidr.fm>
2021-01-03 21:32:15 -0600

    Increased clickable area of navigation links. (#80)

--- src/components/header/Links.js
+++ src/components/header/Links.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import styled from '@emotion/styled';
 import css from '@styled-system/css';
-import { Box, Flex } from 'theme';
+import { Box } from 'theme';
 
 import LiveIcon from '../LiveIcon';
 
@@ -10,12 +10,12 @@ const links = [
   {
     to: '/',
     children: (
-      <Flex alignItems="center">
+      <>
         <LiveIcon size={8} color="red" />
         <Box as="span" ml={2}>
           Live
         </Box>
-      </Flex>
+      </>
     )
   },
   {
@@ -29,37 +29,34 @@ const links = [
   }
 ];
 
-const StyledLinks = styled('nav')({
-  display: 'flex'
-});
+const StyledLinks = styled.nav`
+  display: flex;
+`;
 
-const LinkContainer = styled('div')(
-  css({
-    display: 'inline-block',
-    pr: [5, 4],
+const StyledLink = styled(Link)`
+  align-items: center;
+  display: flex;
+  font-weight: 500;
+  height: 100%;
+  text-transform: uppercase;
+  ${css({
+    mr: [5, 4],
     '&:last-of-type': {
-      pr: 0
+      mr: 0
     }
-  })
-);
-
-const StyledLink = styled(Link)({
-  fontWeight: 500,
-  textTransform: 'uppercase',
-  '&.active': {
-    fontWeight: 700
-  },
-  '&:hover': {
-    textDecoration: 'none'
+  })}
+  &.active {
+    font-weight: 700;
   }
-});
+  &:hover {
+    text-decoration: none;
+  }
+`;
 
 export const Links = (props) => (
   <StyledLinks {...props}>
     {links.map((link) => (
-      <LinkContainer key={link.to}>
-        <StyledLink activeClassName="active" {...link} />
-      </LinkContainer>
+      <StyledLink activeClassName="active" key={link.to} {...link} />
     ))}
   </StyledLinks>
 );
