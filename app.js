547400f090503bff247d774cf945d25a104f447a
Shola Anozie <shola@soulprovidr.fm>
2021-11-21 20:26:05 -0500

    Better play/pause/buffering states.

--- app.js
+++ app.js
@@ -80,11 +80,29 @@ const ListenButton = {
   view({ attrs }) {
     return m(
       "button.listen",
-      { disabled: radio.status === "buffering", onclick: attrs.onclick },
       {
-        buffering: "LOADING",
-        playing: "STOP",
-        stopped: "LISTEN",
+        disabled: radio.status === "buffering",
+        onclick: attrs.onclick,
+      },
+      {
+        buffering: m.trust(`
+          <svg class="loading" xmlns="http://www.w3.org/2000/svg" width="16" height="12" fill="currentColor" class="bi bi-disc-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-6 0a2 2 0 1 0-4 0 2 2 0 0 0 4 0zM4 8a4 4 0 0 1 4-4 .5.5 0 0 0 0-1 5 5 0 0 0-5 5 .5.5 0 0 0 1 0zm9 0a.5.5 0 1 0-1 0 4 4 0 0 1-4 4 .5.5 0 0 0 0 1 5 5 0 0 0 5-5z"/>
+          </svg>
+          LOADING
+        `),
+        playing: m.trust(`
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop-fill" viewBox="0 0 16 16">
+            <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z"/>
+          </svg>
+          STOP
+        `),
+        stopped: m.trust(`
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+          </svg>
+          LISTEN
+        `),
       }[attrs.status]
     );
   },
