20a57e755f82e460bf16290ddb7de51c2632df8b
Shola Anozie <shola@soulprovidr.fm>
2024-02-28 02:39:36 -0500

    Remove displayMode selector.

--- src/components/playlists/components/Controls.tsx
+++ src/components/playlists/components/Controls.tsx
@@ -1,17 +1,10 @@
-import { DisplayMode } from "../types";
 import css from "./Controls.module.scss";
 
 interface IControlsProps {
-  displayMode: "grid" | "list";
-  setDisplayMode: (mode: DisplayMode) => void;
   setFilterTerm: (term: string) => void;
 }
 
-export const Controls = ({
-  displayMode,
-  setDisplayMode,
-  setFilterTerm,
-}: IControlsProps) => (
+export const Controls = ({ setFilterTerm }: IControlsProps) => (
   <div className={css.controls}>
     <fieldset>
       <legend>Filter</legend>
@@ -20,28 +13,6 @@ export const Controls = ({
         type="text"
         onChange={(e) => setFilterTerm(e.target.value)}
         placeholder="Start typing to filter..."
-      />
-    </fieldset>
-    <fieldset>
-      <legend>Display Mode</legend>
-      <input
-        aria-checked={displayMode === DisplayMode.GRID}
-        aria-label="grid"
-        checked={displayMode === DisplayMode.GRID}
-        id="grid"
-        name="display"
-        onChange={(e) => e.target.checked && setDisplayMode(DisplayMode.GRID)}
-        type="checkbox"
-      />
-      <input
-        aria-checked={displayMode === DisplayMode.LIST}
-        aria-label="list"
-        checked={displayMode === DisplayMode.LIST}
-        className={css.checkbox}
-        id="list"
-        name="display"
-        onChange={(e) => e.target.checked && setDisplayMode(DisplayMode.LIST)}
-        type="checkbox"
       />
     </fieldset>
   </div>
