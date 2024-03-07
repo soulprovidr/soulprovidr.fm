d2ece0842591e6bf69de495943e9b37e79f8f714
Shola Anozie <shola@soulprovidr.fm>
2024-03-06 23:10:57 -0500

    Remove unnecessary code.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { MDXProvider } from "@components/mdx";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -9,15 +8,12 @@ interface ILayoutProps {
   children: ReactNode;
 }
 
-export const Layout = ({ children }: ILayoutProps) => {
-  const { asPath } = useRouter();
-  return (
-    <MDXProvider>
-      <div className={css.layout}>
-        <Header />
-        <main className={css.main}>{children}</main>
-        <Footer className={css.footer} />
-      </div>
-    </MDXProvider>
-  );
-};
+export const Layout = ({ children }: ILayoutProps) => (
+  <MDXProvider>
+    <div className={css.layout}>
+      <Header />
+      <main className={css.main}>{children}</main>
+      <Footer className={css.footer} />
+    </div>
+  </MDXProvider>
+);
